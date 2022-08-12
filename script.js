1910d97c7b2cb41d28e0e76c63d24391f7e9bf19
AQ Testing <69691125+aquariustesting@users.noreply.github.com>
2022-08-12 18:31:07 -0400

    Simple Interest Calculator CSS
    Simple Interest Calculator CSS file for test

---
+++ script.js
@@ -0,0 +1,31 @@
+function compute() {
+    //Get the values principal, rate , years and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    var myName = "Juan Rubiell"
+    //Create the Interest text is important to pay attention to the .innerHTML and Marks
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>" + " My name is: " + myName;
+
+}
+
+//update the rate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
